c7ba79f642489d21b2dc2c0556cc8b917a8cc030
sidhubad143 <lakhwinder4332@gmail.com>
2025-12-11 17:45:51 +0530

    Update couple.py

--- plugins/couple.py
+++ plugins/couple.py
@@ -1,192 +1,216 @@
-from datetime import datetime, timedelta
-import pytz
+# Couples.py
+# 1st code with image generation feature added
+
+# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+# Owner https://t.me/ll_BAD_MUNDA_ll
+
 import os
 import random
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ChatType
-from telegraph import upload_file
+from datetime import datetime, timedelta
 from PIL import Image, ImageDraw
-import requests
 
-from utils import get_image, get_couple, save_couple
+from pyrogram import filters
+from pyrogram.enums import ChatAction
+
 from BADMUSIC import app
 
+# <=======================================================================================================================================================================================================================================================================================================================================================================================================================================================================
 
-# get current date in GMT+5:30 timezone
-def get_today_date():
-    timezone = pytz.timezone("Asia/Kolkata")
-    now = datetime.now(timezone)
-    return now.strftime("%d/%m/%Y")
+# <================================================ FUNCTION =======================================================>
 
+def dt():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
 
-# get tomorrow's date in GMT+5:30 timezone
+def dt_tom():
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
 
+tomorrow = str(dt_tom())
+today = str(dt()[0])
 
-def get_todmorrow_date():
-    timezone = pytz.timezone("Asia/Kolkata")
-    tomorrow = datetime.now(timezone) + timedelta(days=1)
-    return tomorrow.strftime("%d/%m/%Y")
+COUPLES_PIC = "https://envs.sh/n4c.jpg"
+C = """
+✧ ᴄᴏᴜᴘʟᴇs ᴏғ ᴅᴀʏ ✧
+❍═══════════════════❍
+{} + ( PGM🎀😶 (https://t.me/BadMundaXD) + 花火 (https://t.me/JXTT_5911) + ゼロツー (https://t.me/PBX_CHAT) ) = 💞
+❍═══════════════════❍
+ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴄᴀɴ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12AM {}
+"""
+CAP = """
+✧ ᴄᴏᴜᴘʟᴇs ᴏғ ᴅᴀʏ ✧
+❍═══════════════════❍
+{} + {} = 💞
+❍═══════════════════❍
+ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴄᴀɴ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12AM {}
+"""
 
+CAP2 = """
+✧ ᴄᴏᴜᴘʟᴇs ᴏғ ᴅᴀʏ ✧
+❍═══════════════════❍
+{} + {} = 💞
+❍═══════════════════❍
+ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴄᴀɴ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12AM {}
+"""
 
-# Download image from URL
-
-
-def download_image(url, path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(path, "wb") as f:
-            f.write(response.content)
-    return path
-
-
-# Dates
-tomorrow = get_todmorrow_date()
-today = get_today_date()
-
-
-@app.on_message(filters.command(["couple", "couples"]))
-async def ctest(_, message):
-    cid = message.chat.id
-    if message.chat.type == ChatType.PRIVATE:
-        return await message.reply_text("Tʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
-
-    p1_path = "downloads/pfp.png"
-    p2_path = "downloads/pfp1.png"
-    test_image_path = f"downloads/test_{cid}.png"
-    cppic_path = "downloads/cppic.png"
-
+async def create_couple_image(client, c1_id, c2_id, chat_id):
+    """Create couple image with profile pictures"""
     try:
-        is_selected = await get_couple(cid, today)
-        if not is_selected:
-            msg = await message.reply_text("❣️")
-            list_of_users = []
-
-            async for i in app.get_chat_members(message.chat.id, limit=50):
-                if not i.user.is_bot and not i.user.is_deleted:
-                    list_of_users.append(i.user.id)
-
-            c1_id = random.choice(list_of_users)
-            c2_id = random.choice(list_of_users)
-            while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-
-            photo1 = (await app.get_chat(c1_id)).photo
-            photo2 = (await app.get_chat(c2_id)).photo
-
-            N1 = (await app.get_users(c1_id)).mention
-            N2 = (await app.get_users(c2_id)).mention
-
-            try:
-                p1 = await app.download_media(photo1.big_file_id, file_name=p1_path)
-            except Exception:
-                p1 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p1_path
-                )
-            try:
-                p2 = await app.download_media(photo2.big_file_id, file_name=p2_path)
-            except Exception:
-                p2 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p2_path
-                )
-
-            img1 = Image.open(p1)
-            img2 = Image.open(p2)
-
-            background_image_path = download_image(
-                "https://envs.sh/_92.jpg", cppic_path
-            )
-            img = Image.open(background_image_path)
-
-            img1 = img1.resize((437, 437))
-            img2 = img2.resize((437, 437))
-
-            mask = Image.new("L", img1.size, 0)
-            draw = ImageDraw.Draw(mask)
-            draw.ellipse((0, 0) + img1.size, fill=255)
-
-            mask1 = Image.new("L", img2.size, 0)
-            draw = ImageDraw.Draw(mask1)
-            draw.ellipse((0, 0) + img2.size, fill=255)
-
-            img1.putalpha(mask)
-            img2.putalpha(mask1)
-
-            draw = ImageDraw.Draw(img)
-
-            img.paste(img1, (116, 160), img1)
-            img.paste(img2, (789, 160), img2)
-
-            img.save(test_image_path)
-
-            TXT = f"""
-**Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ:
-
-{N1} + {N2} = 💚
-
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
-            """
-
-            await message.reply_photo(
-                test_image_path,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="Aᴅᴅ ᴍᴇ 🌋",
-                                url=f"https://t.me/{app.username}?startgroup=true",
-                            )
-                        ]
-                    ]
-                ),
-            )
-
-            await msg.delete()
-            a = upload_file(test_image_path)
-            for x in a:
-                img_url = "https://graph.org/" + x
-                couple = {"c1_id": c1_id, "c2_id": c2_id}
-                await save_couple(cid, today, couple, img_url)
-
-        else:
-            msg = await message.reply_text("❣️")
-            b = await get_image(cid)
-            c1_id = int(is_selected["c1_id"])
-            c2_id = int(is_selected["c2_id"])
-            c1_name = (await app.get_users(c1_id)).first_name
-            c2_name = (await app.get_users(c2_id)).first_name
-
-            TXT = f"""
-**Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ 🎉:
-
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
-
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
-            """
-            await message.reply_photo(
-                b,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="Aᴅᴅ ᴍᴇ🌋",
-                                url=f"https://t.me/{app.username}?startgroup=true",
-                            )
-                        ]
-                    ]
-                ),
-            )
-            await msg.delete()
-
-    except Exception as e:
-        print(str(e))
-    finally:
+        photo1 = (await client.get_chat(c1_id)).photo
+        photo2 = (await client.get_chat(c2_id)).photo
+        
+        # Download profile pictures
         try:
-            os.remove(p1_path)
-            os.remove(p2_path)
-            os.remove(test_image_path)
-            os.remove(cppic_path)
-        except Exception as cleanup_error:
-            print(f"Error during cleanup: {cleanup_error}")
+            p1 = await client.download_media(photo1.big_file_id, file_name=f"pfp_{chat_id}_1.png")
+        except Exception:
+            p1 = "resources/image/C/coupless.png"
+        
+        try:
+            p2 = await client.download_media(photo2.big_file_id, file_name=f"pfp_{chat_id}_2.png")
+        except Exception:
+            p2 = "resources/image/C/coupless.png"
+        
+        # Open images
+        img1 = Image.open(p1)
+        img2 = Image.open(p2)
+        
+        # Select random background
+        xy = ["Bad1", "Bad2", "Bad3"]
+        x = random.choice(xy)
+        img = Image.open(f"resources/image/C/{x}.png")
+        
+        # Resize profile pictures
+        img1 = img1.resize((680, 680))
+        img2 = img2.resize((680, 680))
+        
+        # Create circular masks
+        mask = Image.new('L', img1.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + img1.size, fill=255)
+        
+        mask1 = Image.new('L', img2.size, 0)
+        draw = ImageDraw.Draw(mask1)
+        draw.ellipse((0, 0) + img2.size, fill=255)
+        
+        # Apply masks
+        img1.putalpha(mask)
+        img2.putalpha(mask1)
+        
+        # Paste images on background
+        img.paste(img1, (185, 359), img1)
+        img.paste(img2, (1696, 359), img2)
+        
+        # Save final image
+        output_path = f'couple_{chat_id}.png'
+        img.save(output_path)
+        
+        # Cleanup downloaded files
+        try:
+            if p1 != "resources/image/C/coupless.png":
+                os.remove(p1)
+            if p2 != "resources/image/C/coupless.png":
+                os.remove(p2)
+        except Exception:
+            pass
+        
+        return output_path
+    except Exception as e:
+        print(f"Error creating couple image: {e}")
+        return None
+
+@app.on_cmd(["couple", "couples", "lover", "shipping"])
+async def nibba_nibbi(client, message):
+    if message.from_user.id == 5540249238:
+        my = await client.get_users("rfxtuv")
+        me = await client.get_users(5540249238)
+        await message.reply_photo(
+            photo=COUPLES_PIC, caption=C.format(me.mention, tomorrow)
+        )
+    else:
+        try:
+            chat_id = message.chat.id
+            
+            # Send loading message
+            loading_message = await message.reply_text("❤️")
+            
+            is_selected = await get_couple(chat_id, today)
+            
+            if not is_selected:
+                list_of_users = []
+                async for i in client.get_chat_members(message.chat.id, limit=50):
+                    if not i.user.is_bot:
+                        list_of_users.append(i.user.id)
+                
+                if len(list_of_users) < 2:
+                    await loading_message.delete()
+                    return await message.reply_text("ɴᴏᴛ ᴇɴᴏᴜɢʜ ᴜsᴇʀs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.")
+                
+                c1_id = random.choice(list_of_users)
+                c2_id = random.choice(list_of_users)
+                while c1_id == c2_id:
+                    c1_id = random.choice(list_of_users)
+                
+                c1_mention = (await client.get_users(c1_id)).mention
+                c2_mention = (await client.get_users(c2_id)).mention
+                
+                # Create couple image
+                await client.send_chat_action(chat_id, ChatAction.UPLOAD_PHOTO)
+                image_path = await create_couple_image(client, c1_id, c2_id, chat_id)
+                
+                next_date = (datetime.utcnow() + timedelta(days=1)).strftime("%d/%m/%Y 12 AM")
+                caption = CAP2.format(c1_mention, c2_mention, next_date)
+                
+                if image_path and os.path.exists(image_path):
+                    await message.reply_photo(photo=image_path, caption=caption)
+                    # Cleanup
+                    try:
+                        os.remove(image_path)
+                    except Exception:
+                        pass
+                else:
+                    # Fallback to default image
+                    await message.reply_photo(photo=COUPLES_PIC, caption=caption)
+                
+                await loading_message.delete()
+                
+                couple = {"c1_id": c1_id, "c2_id": c2_id}
+                await save_couple(chat_id, today, couple)
+            
+            elif is_selected:
+                c1_id = int(is_selected["c1_id"])
+                c2_id = int(is_selected["c2_id"])
+                
+                c1_mention = (await client.get_users(c1_id)).mention
+                c2_mention = (await client.get_users(c2_id)).mention
+                
+                # Create couple image
+                await client.send_chat_action(chat_id, ChatAction.UPLOAD_PHOTO)
+                image_path = await create_couple_image(client, c1_id, c2_id, chat_id)
+                
+                next_date = (datetime.utcnow() + timedelta(days=1)).strftime("%d/%m/%Y 12 AM")
+                caption = CAP2.format(c1_mention, c2_mention, next_date)
+                
+                if image_path and os.path.exists(image_path):
+                    await message.reply_photo(photo=image_path, caption=caption)
+                    # Cleanup
+                    try:
+                        os.remove(image_path)
+                    except Exception:
+                        pass
+                else:
+                    # Fallback to default image
+                    await message.reply_photo(photo=COUPLES_PIC, caption=caption)
+                
+                await loading_message.delete()
+                
+        except Exception as e:
+            print(e)
+            await message.reply_text(f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ: {str(e)}")
